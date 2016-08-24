4b2067e168de6b5603bd8e57097339a05513db84
Tony Ramirez <tonyram20@gmail.com>
2016-08-24 13:34:59 -0400

    Starting to add routing with ui-router

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,42 @@
-var app = angular.module("fotballApp", []);
+var app = angular.module('fotballApp', ['ui.router']);
 
-app.controller("MainController", ['$http', '$scope', function($http, $scope) {
-
-    $scope.nameText = "Hey!";
-    $scope.aLeagueIsSelected = false;
-
-    $http({
-        method: 'GET',
-        headers: {
-            'Content-Type': 'text/json',
-            'X-Auth-Token': '1d8e93dbf9104d589b510b458144851b'
-        },
-        url: 'http://api.football-data.org/v1/competitions/?season=2016'
-    }).then(function successCallback(response) {
-        $scope.competitions = response.data;
-    }, function errorCallback(response) {
+app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('leagues', {
+        url: '/leagues',
+        templateUrl: 'views/leagues.html',
+        controller: 'MainController as Ctrl'
     });
 
+    $stateProvider.state('teams', {
+        url: '/teams',
+        templateUrl: 'views/leagues.html',
+        controller: 'MainController as Ctrl'
+    });
+
+});
+
+app.controller('MainController', ['$http', '$scope', function($http, $scope) {
+
+    $scope.nameText = "Hey!";
+    $scope.leaguesLoaded = false;
+    $scope.aLeagueIsSelected = false;
+
+    $scope.getLeagues = function() {
+
+        $http({
+            method: 'GET',
+            headers: {
+                'Content-Type': 'text/json',
+                'X-Auth-Token': '1d8e93dbf9104d589b510b458144851b'
+            },
+            url: 'http://api.football-data.org/v1/competitions/?season=2016'
+        }).then(function successCallback(response) {
+            $scope.competitions = response.data;
+            $scope.leaguesLoaded = true;
+        }, function errorCallback(response) {
+        });
+
+    };
     $scope.getTeams = function(competitionID) {
 
         $http({
@@ -34,5 +54,7 @@ app.controller("MainController", ['$http', '$scope', function($http, $scope) {
         });
 
     };
+
+    $scope.getLeagues();
 
 }]);
